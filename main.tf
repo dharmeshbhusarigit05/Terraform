1347ee0dd743517cc819df41f1c6ea0e78cf5352
dharmeshbhusarigit05 <dharmeshbhusarigithub05@gmail.com>
2025-06-17 18:00:01 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+ region="ap-south-1"
+ }
+
+resource "aws_instance" "one" {
+  ami           = "ami-00b7ea845217da02c"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "dev-server"
+  }
+}
